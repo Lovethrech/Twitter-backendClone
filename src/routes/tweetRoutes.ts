dd6ece0b615d0010dd4c23d633ffe40815504c37
Lovethrech <rachealdolapo45@gmail.com>
2024-05-07 22:40:35 +0100

    seperating the endpoint into a different ts file

--- src/routes/tweetRoutes.ts
+++ src/routes/tweetRoutes.ts
@@ -2,18 +2,18 @@ import { Router } from 'express';
 
 const router = Router();
 
-// USER CRUD 
-// Create User 
+// Tweet CRUD 
+// Create tweet 
 router.post('/', (req, res) => {
     res.status(501).json({ error: "Not implemented" });
 });
 
-// List User 
+// List tweet 
 router.get('/', (req, res) => {
     res.status(501).json({ error: "Not implemented" });
 })
 
-// Get one user 
+// Get one tweet 
 router.get('/:id', (req, res) => {
     const { id } = req.params;
     res.status(501).json({ error: `Not implemented: ${id}` });
@@ -25,7 +25,7 @@ router.put('/:id', (req, res) => {
     res.status(501).json({ error: `Not implemented: ${id}` });
 })
 
-// Delete User 
+// Delete tweet 
 router.delete('/:id', (req, res) => {
     const { id } = req.params;
     res.status(501).json({ error: `Not implemented: ${id}` });
